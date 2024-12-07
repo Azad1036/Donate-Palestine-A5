f88d742d7fef4f9dd370f6edc9ee446267d5cb6e
Azad1036 <mohammadazadhossain2024@gmail.com>
2024-12-07 05:02:44 -0800

    fixed navber and function

--- Js/faq.js
+++ Js/faq.js
@@ -2,12 +2,14 @@ const blog = document.getElementById("blog-btn");
 const home = document.getElementById("home-btn");
 const mainPages = document.getElementById("main-page");
 const faqBody = document.getElementById("faq-body");
+const donateData = document.getElementById("donate-data");
 
 document.getElementById("blog-btn").addEventListener("click", function () {
   blog.classList.add("hidden");
   home.classList.remove("hidden");
   mainPages.classList.add("hidden");
   faqBody.classList.remove("hidden");
+  donateData.classList.add("hidden");
 });
 
 document.getElementById("home-btn").addEventListener("click", function () {
@@ -15,4 +17,5 @@ document.getElementById("home-btn").addEventListener("click", function () {
   home.classList.add("hidden");
   mainPages.classList.remove("hidden");
   faqBody.classList.add("hidden");
+  donateData.classList.remove("hidden");
 });
